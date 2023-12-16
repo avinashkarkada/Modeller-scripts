c6e721c5461609c2ff55a8e9495e8595734222e2
avinashkarkada <avikarkada@gmail.com>
2023-12-17 00:33:39 +0530

    Update cal_DOPE.py

--- cal_DOPE.py
+++ cal_DOPE.py
@@ -1,6 +1,13 @@
 from modeller import *
 from modeller.scripts import complete_pdb
 import os
+from tabulate import tabulate
+
+def list_to_sorted_table(data):
+  headers = ["PDB File", "Score"]
+  sorted_data = sorted(data, key=lambda x: x[1])
+  formatted_data = tabulate(sorted_data, headers=headers, floatfmt=".4f", tablefmt="fancy_grid")
+  return formatted_data
 
 env = Environ()
 env.io.atom_files_directory = ['.', '../atom_files']
@@ -21,4 +28,5 @@ for pdb_file in pdb_files:
     final_dope.append(fin)
 
 
-print(final_dope)
+table = list_to_sorted_table(final_dope)
+print(table)
